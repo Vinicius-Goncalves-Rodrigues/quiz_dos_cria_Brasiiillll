99af9d9ece5e278599a7f7badf0530a64e49e803
T1NT4 <thiagogabriel.osil@gmail.com>
2024-08-28 22:35:42 -0300

    sei la nem lembro qo eu fiz

--- testenoiteedia.php
+++ testenoiteedia.php
@@ -7,7 +7,13 @@
     <link rel="stylesheet" href="testenoiteedia.css">
 </head>
 <body>
-    
+    <header>
+        <a href="indexplay.html">< voltar</a>
+        <div>
+            <h3>vez do:</h3>
+            <h1>TIME SOL</h1>
+        </div>
+    </header>
     <div class="sun-container">
         <div class="sun">
             <img src="star.svg">
